2f2cc9f18034b6fe1087cba3a6546c1112297d35
Pugdan <bogdanbroicea33@gmail.com>
2024-02-14 22:54:42 +0100

    Creacion de otro item despues de probar a hacer un Push a rama TST

--- src/main/java/net/pugdan/pruebamod/item/ModItems.java
+++ src/main/java/net/pugdan/pruebamod/item/ModItems.java
@@ -14,6 +14,9 @@ public class ModItems {
     public static final RegistryObject<Item> SPPHIRE = ITEMS.register("sapphire",
             () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> CORINDON = ITEMS.register("corindon",
+            () -> new Item(new Item.Properties()));
+
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
